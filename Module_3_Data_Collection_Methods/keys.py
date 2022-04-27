d73c26bc20f1fb164e50ba497447d0a0a3880cf6
N. Gizem Bacaksizlar Turbic <39707690+ngizembacaksizlar@users.noreply.github.com>
2022-04-27 09:56:37 +0200

    Add files via upload

---
+++ Module_3_Data_Collection_Methods/keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+apikey ='fzTtHkLiy2zzQ7ZO2EZTmrSWC' #25 alphanumeric characters
+apisecretkey ='8i7tmiyezGcbQlTDbzpmuUc5UmzgYJgsVVxbY3hzHawhUtIxKq'
+accesstoken ='1485987858-ZgBdzePI3LvIktf1xlFw8qHycUQMxOBWFSANwcK'
+accesstokensecret = 'm1y3OpTRD8dJU6BTrWN72m4gKNlwWqsgva5EXsuarHDrJ'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOKnSAEAAAAAN057pvIv%2BLdfk9Fn2koEXbLvfOk%3DdxYn2pTeI5wPq66g50pydBptMiVP2XmCaMHRkDHh1H0HW9EFjp'
+
